606ed52bc046b9c567943645b76839b6cf64a828
IslaBecker91031015 <islabecker@gmail.com>
2019-07-11 02:58:46 +1200

    BoxTest Class: canFit, remainingCapacity, addProduct, getWeight methods for 2 boxes tested. PackerTest Class: packedBoxes method tested with 2 Manifests.

--- IT6039_Project/test/packer/BoxTest.java
+++ IT6039_Project/test/packer/BoxTest.java
@@ -12,19 +12,31 @@ public class BoxTest {
 
     // Test data
     Coordinates testCoordinates1 = new Coordinates(3,4);
+    Coordinates testCoordinates2 = new Coordinates(30,100);
+
     Address testAddress1 = new Address("1 First St", "", "Citadel City", "A111", testCoordinates1);
-    Customer testC1 = new Customer("Sally Perry", (testAddress1));
-    Depot testD1 = new Depot("Test Depot", testAddress1);
+    Address testAddress2 = new Address("10 Smith St", "", "Campbell City", "A242", testCoordinates2);
+
+    Customer testC1 = new Customer("Sally Perry", testAddress1);
+    Customer testC2 = new Customer("Sarah Kate", testAddress2);
+
+    Address depotAddress = new Address("2 Depot St", "Kelburn", "Tall City", "A121", testCoordinates1);
+    Depot testD1 = new Depot("Test Depot", depotAddress);
 
     Product p1 = new Product("Saw", 4, true, false);
     Product p2 = new Product("Ladder", 10, true, false);
     Product p3 = new Product("Product1", 22, false, false);
-    Product p4 = new Product("Product2", 10, true, false);
+    Product p4 = new Product("Toolbox", 6, true, false);
     Product p5 = new Product("LightBulbs", 2, false, true);
+
+
+    Box b1 = new Box(testC1, testD1, 20);
+    Box b2 = new Box(testC2, testD1, 30);
 
     @BeforeClass
     public static void setUpClass() {
         System.out.println("Testing Box class...");
+
     }
 
 
@@ -34,14 +46,41 @@ public class BoxTest {
     @Test
     public void testCanFit() {
 
-        System.out.println("canFit: ");
+        System.out.println("Can Fit");
 
-        Box b1 = new Box(testC1, testD1, 20);
         b1.addProduct(p1);
         assertTrue(b1.canFit(p1));
-        assertTrue(b1.canFit(p2));
         assertFalse(b1.canFit(p3));
         assertTrue(b1.canFit(p4));
+
+        b2.addProduct(p1);
+        assertTrue(b1.canFit(p1));
+        assertFalse(b1.canFit(p3));
+        assertTrue(b1.canFit(p4));
+
+    }
+
+    /**
+     * Testing of addProduct method of Box class.
+     */
+    @Test
+    public void addProduct() {
+
+        System.out.println("Add Product");
+
+        b1.addProduct(p1);
+        assertTrue(b1.getLabel().contains(p1.getName()));
+        b1.addProduct(p2);
+        assertTrue(b1.getLabel().contains(p2.getName()));
+        b1.addProduct(p3);
+        assertFalse(b1.getLabel().contains(p3.getName()));
+
+        b2.addProduct(p2);
+        assertTrue(b2.getLabel().contains(p2.getName()));
+        b2.addProduct(p4);
+        assertTrue(b2.getLabel().contains(p2.getName()));
+        b2.addProduct(p1);
+        assertTrue(b2.getLabel().contains(p2.getName()));
 
     }
 
@@ -52,18 +91,21 @@ public class BoxTest {
     @Test
     public void testGetWeight() {
 
-        System.out.println("Weight: ");
-
-        Box b1 = new Box(testC1, testD1, 20);
+        System.out.println("Get Weight");
 
         b1.addProduct(p1);
         assertEquals(4, b1.getWeight(), 0);
-
         b1.addProduct(p2);
         assertEquals(14, b1.getWeight(), 0);
-
         b1.addProduct(p5);
         assertEquals(16, b1.getWeight(), 0);
+
+        b2.addProduct(p4);
+        assertEquals(6, b2.getWeight(), 0);
+        b2.addProduct(p2);
+        assertEquals(16, b2.getWeight(), 0);
+        b2.addProduct(p1);
+        assertEquals(20, b2.getWeight(), 0);
 
     }
 
@@ -74,16 +116,21 @@ public class BoxTest {
     @Test
     public void testRemainingCapacity() {
 
-        System.out.println("remainingCapacity: ");
-
-        Box b1 = new Box(testC1, testD1, 20);
+        System.out.println("Remaining Capacity");
 
         b1.addProduct(p1);
         b1.addProduct(p2);
         assertEquals(6, b1.remainingCapacity(), 0);
-
         b1.addProduct(p5);
         assertEquals(4, b1.remainingCapacity(), 0);
 
+        b2.addProduct(p2);
+        b2.addProduct(p4);
+        assertEquals(14, b2.remainingCapacity(), 0);
+        b2.addProduct(p1);
+        assertEquals(10, b2.remainingCapacity(), 0);
+
     }
+
 }
+
